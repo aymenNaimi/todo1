1b7891c7dab435922950ce3dcbe8904296f85f97
Aymen Naimi <aymen.naimi.1990@gmail.com>
2015-03-24 13:16:50 +0100

    login and logout with passport

--- app.js
+++ app.js
@@ -3,6 +3,12 @@
  */
 
 var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
+var session = require('express-session');
+var express = require('express');
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+
 mongoose.connect('mongodb://localhost/dbtodo1');
 
 var db = mongoose.connection;
@@ -10,62 +16,102 @@ db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function (callback) {
     console.log('conection opened');
 });
-Schema = mongoose.Schema;
+
+var Schema = mongoose.Schema;
 
 var TodoSchema = new Schema({
     title: String,
     description: String
-
 });
 
 var Todo = mongoose.model('Todo', TodoSchema);
 
-
-var bodyParser = require('body-parser');
-var express = require('express');
 var app = express();
 app.use(bodyParser.json());
+app.use(session({
+    saveUninitialized: true,
+    resave: true,
+    secret: 'secret123'
+}));
 
+app.use(passport.initialize());
+app.use(passport.session());
 
-app
-    .get('/todo', function (req, res) {
-    Todo.find(function (err, todos) {
-        if (err) {
-            res.json(400, "");
+passport.use(new LocalStrategy({
+    usernameField: 'title',
+    passwordField: 'description'
+}, function (username, password, done) {
+    Todo.findOne({
+        title: username
+    }, function (err, user) {
+        if (err) return done(err);
+
+        if (!user || user.description !== password) {
+            return done(null, false, {
+                message: 'Invalid username or password'
+            });
         }
-        else if (!todos) {
-            return res.json(404, todos);
-        }
-        else {
-            res.json(200, todos);
-        }
+
+        return done(null, user);
+    });
+}));
+
+passport.serializeUser(function (user, done) {
+    done(null, user.id);
+});
+
+passport.deserializeUser(function (id, done) {
+    Todo.findOne({
+        _id: id
+    }, function (err, user) {
+        done(err, user);
     });
 });
 
 app
-    .post('/todo', function (req, res) {
-    var todo = new Todo({ title: req.body.title, description: req.body.description });
-
-    todo.save(function (err, silence) {
-        if (err) {
-            res.json(400, "");
-        }
-        else if (!(todo.title && todo.description)) {
-            return res.json(400, "put tile and description");
-        }
-        else {
-            res.json(201, todo);
-        }
-
+    .get('/todos', function(req, res, next) {
+        if (!req.user)
+            return res.send(401);
+        else
+            next();
+    }, function (req, res) {
+        Todo.find(function (err, todos) {
+            if (err) {
+                res.json(400, "");
+            }
+            else if (!todos) {
+                return res.json(404, todos);
+            }
+            else {
+                res.json(200, todos);
+            }
+        });
     });
-});
+
+app
+    .post('/todos', function (req, res) {
+        var todo = new Todo({ title: req.body.title, description: req.body.description });
+
+        todo.save(function (err, silence) {
+            if (err) {
+                res.json(400, "");
+            }
+            else if (!(todo.title && todo.description)) {
+                return res.json(400, "put tile and description");
+            }
+            else {
+                res.json(201, todo);
+            }
+
+        });
+    });
 
 
 app
-    .route('/todo/:id').get(function (req, res) {
+    .route('/todos/:id').get(function (req, res) {
         Todo.findOne({_id: req.params.id}, function (err, todo) {
             if (err) {
-                return res.json(400,"");
+                return res.json(400, "");
             }
             else {
                 if (!todo) {
@@ -111,6 +157,27 @@ app
         });
     });
 
+app
+    .post('/login', function (req, res, next) {
+        passport.authenticate('local', function (err, user) {
+            if (err)
+                return next(err);
+
+            if (!user)
+                return res.sendStatus(401);
+
+            req.logIn(user, function (err) {
+                if (err)
+                    return next(err);
+
+                return res.send();
+            });
+        })(req, res, next);
+    })
+    .get('/logout', function(req, res) {
+        req.logout();
+        res.send();
+    });
 
 var server = app.listen(3000, function () {
 
@@ -119,4 +186,3 @@ var server = app.listen(3000, function () {
 
     console.log('Example app listening at http://%s:%s', host, port);
 });
-
