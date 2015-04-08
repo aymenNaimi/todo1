5f49fe28ba12249604279a86a416a64bbfd2c16b
Aymen Naimi <aymen.naimi.1990@gmail.com>
2015-04-08 12:06:37 +0100

    Reformat code.

--- public/app.js
+++ public/app.js
@@ -1,23 +1,4 @@
 var app = angular.module('myApp',
     ['ngRoute']);
-/*
-app.run(function($rootScope,$http) {
-    $rootScope.logout = function () {
-        $http({
-            method: 'GET',
-            url: 'http://localhost:3000/users/logout',
-            headers: {
-                'Content-Type': 'application/json'
-            }
-        }).success(function (data, status) {
-            if (status === 200) {
-                console.log(" logout success ");
-                self.todos = [];
-                $rootScope.connected= false;
-            }
-        });
-    };
-    $rootScope.connected= false;
-});
-*/
+
 
