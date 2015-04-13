c45aeaea630efc38e7a31af16f6e68d60d2b2614
Aymen Naimi <aymen.naimi.1990@gmail.com>
2015-04-13 16:22:27 +0100

    add login controller test.

---
+++ public/test/controllerlogintest.js
@@ -0,0 +1,18 @@
+describe('Testing Articles Controller', function () {
+    var _scope, ArticlesController;
+    beforeEach(function () {
+        module('myApp');
+        inject(function ($rootScope, $controller) {
+            _scope = $rootScope.$new();
+            ArticlesController = $controller('myCtrl2', {
+                $scope: _scope
+            });
+        });
+    });
+    it('Should be registered', function () {
+        expect(ArticlesController).toBeDefined();
+    });
+    it('Should connect methods', function () {
+        expect(ArticlesController.connect).toBeDefined();
+    });
+});
--- public/test/routes.js
+++ public/test/routes.js
@@ -1,15 +1,11 @@
-describe('Testing login Routing', function() {
+describe('Testing login Routing', function () {
     beforeEach(module('myApp'));
-    it('Should map a "list" route', function() {
-        inject(function($route) {
+    it('Should map a "list" route', function () {
+        inject(function ($route) {
             expect($route.routes['/login'].templateUrl).
                 toEqual('api/login/login.html');
             expect($route.routes['/todos'].templateUrl).
                 toEqual('api/todos/todos.html');
-
-
-
-
         });
     });
 });
