49ea6154208892a1c9e0bac2aefc24a0d3ce8cab
Aymen Naimi <aymen.naimi.1990@gmail.com>
2015-04-14 17:43:51 +0100

    Reformat code .

--- public/test/todoscontroller.js
+++ public/test/todoscontroller.js
@@ -13,49 +13,22 @@ describe('PasswordController', function () {
                 };
             }
         });
-
-
         var $controller = $injector.get('$controller');
         $httpBackend = $injector.get('$httpBackend');
         $rootScope = $injector.get('$rootScope');
-
-        // var  $service =$injector.get('$service');
-
-        // The injector unwraps the underscores (_) from around the parameter names when matching
         createController = function () {
             return $controller('myCtrl', {'$scope': $rootScope, loggedin: {username: 'aymen', password: 'allmas', _id: '5527d02e1d0a2ee9d14f5307' } });
         };
-        /*
-         createService = function() {
-         return $service('authService', {'$scope' : $rootScope });
-         };
-         */
-
     }));
-    describe('add todo test', function () {
-        it(' adding todo', function () {
-
+    describe('get todo test', function () {
+        it('get todo test', function () {
             var todo = {title: 'alooo', description: 'helo', done: false};
-            //controller.titleup ='ayyyymen';
-            //controller.descriptionup ='allmas';
             var todos = [todo];
             $httpBackend.expectGET('/todos/loggedin').respond(200);
-
             $httpBackend.expectGET('http://localhost:3000/todos/').respond(200, todos);
-//var data = {  title : controller.titleup , description :controller.descriptionup , done : false } ;
-            //    var todosend = {  title : controller.titleup , description :controller.descriptionup  };
-
-            // controller.todos.push(data);
-            //$httpBackend.expectPOST('http://localhost:3000/todos/',todosend).respond(200);
-            //controller.add();
-
-
-            //expect(controller.todos).toBe([]) ;
             var controller = createController();
             $httpBackend.flush();
             expect(controller.todos).toEqualData(todos);
-
-            //  expect(todos[0].description).toBe(controller.description) ;
         });
     });
 });
