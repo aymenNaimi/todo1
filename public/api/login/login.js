46701057647364df730cdcc5e84dc3954db67d8a
Aymen Naimi <aymen.naimi.1990@gmail.com>
2015-04-14 15:05:46 +0100

    Reformat code .

--- public/api/login/login.js
+++ public/api/login/login.js
@@ -1,8 +1,6 @@
 angular.module('myApp').controller('myCtrl2', function ($scope, $http, $location,$rootScope) {
     var self = this;
     self.connect = function () {
-        self.ctd=false ;
-        console.log('*********aaa**********');
         $http({
             method: 'POST',
             url: 'http://localhost:3000/users/login',
@@ -10,8 +8,6 @@ angular.module('myApp').controller('myCtrl2', function ($scope, $http, $location
                 'Content-Type': 'application/json'
             }, data: {username: self.username, password: self.password}
         }).success(function (data, status) {
-            console.log('*********bbb**********');
-            self.ctd=true ;
             $rootScope.connected=  true;
             $location.path('/todos');
         });
--- public/test/unittest.js
+++ public/test/unittest.js
@@ -1,37 +1,7 @@
 describe('PasswordController', function() {
     beforeEach(module('myApp'));
-
     var $controller ,result,$httpBackend,$rootScope, createController;
-
-    beforeEach(function (done) {
-        window.jasmine.DEFAULT_TIMEOUT_INTERVAL = 10000;
-        setTimeout(function () {
-            console.log('inside timeout');
-            done();
-        }, 500);
-    });
-/*
-    it('passes', function () {
-        expect({}).toBeDefined();
-    });
-*/
         beforeEach(inject(function($injector){
-
-
-            jasmine.addMatchers({
-                toEqualData : function(util, customEqualityTesters) {
-                    return {
-                        compare: function(actual, expected) {
-                            return {
-                                pass: angular.equals(actual, expected)
-                            };
-                        }
-                    };
-                }
-            });
-
-
-
         var $controller = $injector.get('$controller');
         $httpBackend = $injector.get('$httpBackend');
         $rootScope = $injector.get('$rootScope');
@@ -40,24 +10,16 @@ describe('PasswordController', function() {
             return $controller('myCtrl2', {'$scope' : $rootScope });
         };
     }));
-
     describe('$scope.connect', function() {
         it(' testing that some variable are clear', function() {
             var controller = createController();
-
              controller.username ='ayyyymen';
              controller.password ='allmas';
-
             $httpBackend.expectGET('/todos/loggedin').respond(200);
-
-console.log('aaaaaaaaaaaaaaaa');
             $httpBackend.expectPOST('http://localhost:3000/users/login',{username : controller.username , password :controller.password  }).respond(200);
-            console.log('bbbbbbbbbbbbbbb');
             controller.connect();
-            console.log('ccccccccccccc');
             $httpBackend.flush();
-            console.log('ddddddddddddd');
-            expect(controller.ctd).toBe(true);
+            expect($rootScope.connected).toBe(true) ;
 
         });
     });
