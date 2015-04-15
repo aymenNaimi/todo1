784862fcdea56f94ce1aad3deec5bd2b5ac864f1
Aymen Naimi <aymen.naimi.1990@gmail.com>
2015-04-15 09:10:58 +0100

    Add update todo test.

--- public/test/todoscontroller.js
+++ public/test/todoscontroller.js
@@ -45,5 +45,40 @@ describe('PasswordController', function () {
         $httpBackend.flush();
         expect(controller.todos).toEqualData(added);
     });
+    /*
+     it('delete todo test', function () {
+     var todo = {title: 'todo11', description: 'desc11', done: false, _id : "5527d0618e3702d1321a319a" };
+     var todos = [todo];
+     var after = [];
+     $httpBackend.expectGET('/todos/loggedin').respond(200);
+     $httpBackend.expectGET('http://localhost:3000/todos/').respond(200, todos);
+
+     $httpBackend.expectDELETE('http://localhost:3000/todos/'+"5527d0618e3702d1321a319a" ).respond(200, todo);
+
+     var controller = createController();
+     //  controller.delete("5527d0618e3702d1321a319a" );
+     spyOn(window, 'confirm').and.returnValue(true) ;
+     $httpBackend.flush();
+     expect(controller.todos).toEqualData(after);
+     });
+     */
+    it('update todo test', function () {
+        var todo = {title: 'todo11', description: 'desc11', done: false, _id: "5527d0618e3702d1321a319a"};
+        var updated = {title: 'todo1122', description: 'desc1133', done: true};
+        var updatedid = {title: 'todo1122', description: 'desc1133', done: true, _id: "5527d0618e3702d1321a319a"};
+        var array = [updatedid];
+        var todos = [todo];
+        $httpBackend.expectGET('/todos/loggedin').respond(200);
+        $httpBackend.expectGET('http://localhost:3000/todos/').respond(200, todos);
+        $httpBackend.expectPUT('http://localhost:3000/todos/' + todo._id, updated).respond(200, updatedid);
+        var controller = createController();
+        controller.idup = updatedid._id;
+        controller.titleup = updatedid.title;
+        controller.descriptionup = updatedid.description;
+        controller.doneup = updatedid.done;
+        controller.update2();
+        $httpBackend.flush();
+        expect(controller.todos).toEqualData(array);
+    });
 });
 
