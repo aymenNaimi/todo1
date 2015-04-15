8706d059bf3107ff35209c69cfa3f2cd059222e7
Aymen Naimi <aymen.naimi.1990@gmail.com>
2015-04-15 18:26:05 +0100

    Reformat code.

--- public/e2etests/login.e2e.tests.js
+++ public/e2etests/login.e2e.tests.js
@@ -1,37 +1,12 @@
-describe('todos E2E Tests:', function() {
-    /*
-    before(inject(function($injector){
-        var $controller = $injector.get('$controller');
-        $httpBackend = $injector.get('$httpBackend');
-        $rootScope = $injector.get('$rootScope');
-        createController = function() {
-            return $controller('myCtrl2', {'$scope' : $rootScope });
-        };
-    }));
-
-*/
-
-
-    describe('login', function() {
-        it('Should not be able to login', function() {
+describe('todos E2E Tests:', function () {
+    describe('login', function () {
+        it('Should not be able to login', function () {
             browser.get('http://localhost:3000/#/login');
             element(by.model('mycl.username')).sendKeys('aymen');
             element(by.model('mycl.password')).sendKeys('allmas');
             element(by.css('button[type=submit]')).click();
-          //  element(by.css(':button')).click();
-           /*
-            element(by.binding('error')).getText().then(function(errorText)
-            {
-                expect(errorText).toBe('User is not logged in');
-            });
-            */
             expect('aaa').toBe('aaa');
-
-
-
-
             expect(browser.getCurrentUrl()).toEqual('http://localhost:3000/#/todos');
-        //    expect($location.path).toBe('http://localhost:3000/#/todos');
         });
     });
 });
