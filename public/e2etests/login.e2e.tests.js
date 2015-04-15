cc45127cc9f7dc6e3f3a1ba3c948473fd15d2459
Aymen Naimi <aymen.naimi.1990@gmail.com>
2015-04-15 18:45:03 +0100

    Add test can't get todos without login.

--- public/e2etests/login.e2e.tests.js
+++ public/e2etests/login.e2e.tests.js
@@ -1,6 +1,12 @@
 describe('todos E2E Tests:', function () {
     describe('login', function () {
-        it('Should not be able to login', function () {
+        it('Should not be able to get todos', function () {
+            browser.get('http://localhost:3000/#/todos');
+
+            expect('aaa').toBe('aaa');
+            expect(browser.getCurrentUrl()).toEqual('http://localhost:3000/#/login');
+        });
+        it('Should  able to login', function () {
             browser.get('http://localhost:3000/#/login');
             element(by.model('mycl.username')).sendKeys('aymen');
             element(by.model('mycl.password')).sendKeys('allmas');
