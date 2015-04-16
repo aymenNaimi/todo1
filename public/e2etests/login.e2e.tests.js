bbdc9c8990a8713ab59e2f45489cd68c0808196a
Aymen Naimi <aymen.naimi.1990@gmail.com>
2015-04-16 16:09:59 +0100

    Reformat code.

--- public/e2etests/login.e2e.tests.js
+++ public/e2etests/login.e2e.tests.js
@@ -1,8 +1,6 @@
 describe('todos E2E Tests:', function () {
 
 
-
-
     describe('login logout test', function () {
 
         it('Should not be able to get todos ', function () {
@@ -11,11 +9,10 @@ describe('todos E2E Tests:', function () {
         });
         it('Should not be able to go to the home page', function () {
             browser.get('http://localhost:3000/#/login');
-
             element(by.linkText('Home')).click();
             expect(browser.getCurrentUrl()).toEqual('http://localhost:3000/#/login');
         });
-        it('Should  able to login', function () {
+        it('Should  able to login and logout', function () {
             browser.get('http://localhost:3000/#/login');
             element(by.model('mycl.username')).sendKeys('aymen');
             element(by.model('mycl.password')).sendKeys('allmas');
@@ -46,112 +43,59 @@ describe('todos E2E Tests:', function () {
             element(by.model('mycl.descriptionup')).sendKeys('407');
             element(by.buttonText("save")).click();
         });
-
-
-   //     protractor.By.repeater("l in mycl.todos");
-
         it('testing that the added todo was found in the todos model', function () {
-
             browser.get('http://localhost:3000/#/login');
             element(by.model('mycl.username')).sendKeys('aymen');
             element(by.model('mycl.password')).sendKeys('allmas');
             element(by.css('button[type=submit]')).click();
-
             expect(browser.getCurrentUrl()).toEqual('http://localhost:3000/#/todos');
-            element.all(by.repeater('l in mycl.todos').column('title')).then(function(elems) {
-                expect( elems[1].getText()).toEqual('peugeot');
+            element.all(by.repeater('l in mycl.todos').column('title')).then(function (elems) {
+                expect(elems[1].getText()).toEqual('peugeot');
             });
-
-            element.all(by.repeater('l in mycl.todos').column('description')).then(function(elems) {
-                expect( elems[1].getText()).toEqual('407');
+            element.all(by.repeater('l in mycl.todos').column('description')).then(function (elems) {
+                expect(elems[1].getText()).toEqual('407');
             });
-            element.all(by.repeater('l in mycl.todos').column('done')).then(function(elems) {
-                expect( elems[1].getText()).toEqual('false');
+            element.all(by.repeater('l in mycl.todos').column('done')).then(function (elems) {
+                expect(elems[1].getText()).toEqual('false');
             });
         });
-
-
-
-
         it('testing done todo', function () {
-
             browser.get('http://localhost:3000/#/login');
             element(by.model('mycl.username')).sendKeys('aymen');
             element(by.model('mycl.password')).sendKeys('allmas');
             element(by.css('button[type=submit]')).click();
-
             expect(browser.getCurrentUrl()).toEqual('http://localhost:3000/#/todos');
-
-            element.all(by.repeater('l in mycl.todos')).then(function(elems) {
-
+            element.all(by.repeater('l in mycl.todos')).then(function (elems) {
                 elems[1].element(by.css('button')).click();
-
             });
-
         });
-
-
-
-
-        it('testing done todo to be false', function () {
-
+        it('testing update todo', function () {
             browser.get('http://localhost:3000/#/login');
             element(by.model('mycl.username')).sendKeys('aymen');
             element(by.model('mycl.password')).sendKeys('allmas');
             element(by.css('button[type=submit]')).click();
-
             expect(browser.getCurrentUrl()).toEqual('http://localhost:3000/#/todos');
-
-            element.all(by.repeater('l in mycl.todos')).then(function(elems) {
-
-                elems[1].all(by.css('button')).then(function(elem) {
-                     elem[1].click();
+            element.all(by.repeater('l in mycl.todos')).then(function (elems) {
+                elems[1].all(by.css('button')).then(function (elem) {
+                    elem[1].click();
                     element(by.model('mycl.descriptionup')).clear();
                     element(by.model('mycl.descriptionup')).sendKeys('309');
                     element(by.buttonText("save")).click();
-
-
-
-
                 });
-
             });
-
         });
-
         it('testing delete todo', function () {
-
             browser.get('http://localhost:3000/#/login');
             element(by.model('mycl.username')).sendKeys('aymen');
             element(by.model('mycl.password')).sendKeys('allmas');
             element(by.css('button[type=submit]')).click();
-
             expect(browser.getCurrentUrl()).toEqual('http://localhost:3000/#/todos');
-
-            element.all(by.repeater('l in mycl.todos')).then(function(elems) {
-
-                elems[0].all(by.css('button')).then(function(elem) {
+            element.all(by.repeater('l in mycl.todos')).then(function (elems) {
+                elems[0].all(by.css('button')).then(function (elem) {
                     elem[2].click();
-
-
-
                     browser.switchTo().alert().accept();
-
                 });
-
             });
-
         });
-
-
-
-
-
-
-
-
-
-
-
     });
 });
