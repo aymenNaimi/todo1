3bbd9c8ffa33cb42ddb493aaa4ea2a13b97e7b90
Aymen Naimi <aymen.naimi.1990@gmail.com>
2015-04-16 15:26:28 +0100

    Testing update todo.

--- public/e2etests/login.e2e.tests.js
+++ public/e2etests/login.e2e.tests.js
@@ -87,5 +87,42 @@ describe('todos E2E Tests:', function () {
 
 
 
+
+        it('testing done todo to be false', function () {
+
+            browser.get('http://localhost:3000/#/login');
+            element(by.model('mycl.username')).sendKeys('aymen');
+            element(by.model('mycl.password')).sendKeys('allmas');
+            element(by.css('button[type=submit]')).click();
+
+            expect(browser.getCurrentUrl()).toEqual('http://localhost:3000/#/todos');
+
+            element.all(by.repeater('l in mycl.todos')).then(function(elems) {
+
+                elems[1].all(by.css('button')).then(function(elem) {
+                     elem[1].click();
+                    element(by.model('mycl.descriptionup')).clear();
+                    element(by.model('mycl.descriptionup')).sendKeys('309');
+                    element(by.buttonText("save")).click();
+
+
+
+
+                });
+
+            });
+
+        });
+
+
+
+
+
+
+
+
+
+
+
     });
 });
