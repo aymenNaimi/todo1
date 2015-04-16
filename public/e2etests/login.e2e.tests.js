b6978c65a108feaf230bcfcc998141b228780981
Aymen Naimi <aymen.naimi.1990@gmail.com>
2015-04-16 10:11:27 +0100

    Testing title tobe Todo Management.

--- public/e2etests/login.e2e.tests.js
+++ public/e2etests/login.e2e.tests.js
@@ -6,6 +6,7 @@ describe('todos E2E Tests:', function () {
         });
         it('Should not be able to go to the home page', function () {
             browser.get('http://localhost:3000/#/login');
+
             element(by.linkText('Home')).click();
             expect(browser.getCurrentUrl()).toEqual('http://localhost:3000/#/login');
         });
@@ -17,6 +18,16 @@ describe('todos E2E Tests:', function () {
             expect(browser.getCurrentUrl()).toEqual('http://localhost:3000/#/todos');
             element(by.linkText('logout')).click();
             expect(browser.getCurrentUrl()).toEqual('http://localhost:3000/#/login');
+
+        });
+        it('testing title to be Togo Management', function () {
+            browser.get('http://localhost:3000/#/login');
+            expect(browser.getTitle()).toBe('Todo Management');
+            element(by.model('mycl.username')).sendKeys('aymen');
+            element(by.model('mycl.password')).sendKeys('allmas');
+            element(by.css('button[type=submit]')).click();
+            expect(browser.getCurrentUrl()).toEqual('http://localhost:3000/#/todos');
+            expect(browser.getTitle()).toBe('Todo Management');
         });
     });
 });
